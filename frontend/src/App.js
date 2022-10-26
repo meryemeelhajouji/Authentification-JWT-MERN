acd7b7b9cb415f1e9308da99cb0f6ded6a533bf9
meryemeelhajouji <meryemelhajouji.99@gmail.com>
2022-10-26 16:23:02 +0200

    create component login

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Login from  './composents/auth/login'
+
+function App() {
+  return (
+    <div className="App">
+     <Login/>
+    </div>
+  );
+}
+
+export default App;
